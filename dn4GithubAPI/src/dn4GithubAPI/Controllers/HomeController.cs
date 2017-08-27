0d57284d158e19529d2885ec59bb9d9646d64088
Andrew Glines <aglines-epicodus@gmail.com>
2017-08-27 12:44:29 -0700

    Added everything else

--- dn4GithubAPI/src/dn4GithubAPI/Controllers/HomeController.cs
+++ dn4GithubAPI/src/dn4GithubAPI/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using dn4GithubAPI.Models;
+using Newtonsoft.Json.Linq;
 
 namespace dn4GithubAPI.Controllers
 {
@@ -21,8 +23,9 @@ namespace dn4GithubAPI.Controllers
 
         public IActionResult Projects()
         {
+            JObject starredRepos = Github.GetStarredRepos();
             ViewData["Message"] = "Your projects page.";
-            return View();
+            return View(starredRepos);
         }
 
         public IActionResult Error()
---
+++ dn4GithubAPI/src/dn4GithubAPI/Models/Github.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace dn4GithubAPI.Models
+{
+    public class Github
+    {
+        public int stargazers_count { get; set; }
+
+
+        public static JObject GetStarredRepos()
+        {
+            var client = new RestClient("https://api.github.com");
+            var request = new RestRequest("/search/repositories?q=user:aglines-epicodus&sort=stars&order=desc", Method.GET);
+            request.AddHeader("User-Agent", "aglines-epicodus");
+
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+
+            return jsonResponse;
+        }
+
+
+
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+
+    }
+}
